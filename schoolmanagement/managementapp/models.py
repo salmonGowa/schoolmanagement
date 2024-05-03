bf5a9754b792e070dc7052456afb0017787917ea
salmonGowa <gowasalmon@yahoo.com>
2024-05-03 11:59:04 -0400

    models

--- schoolmanagement/managementapp/models.py
+++ schoolmanagement/managementapp/models.py
@@ -3,4 +3,93 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name=models.CharField(max_length=50)
+
+#admmin
+class AdminHOD(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    password=models.CharField(max_length=40)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class Staffs(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    password=models.CharField(max_length=40)
+    address=models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
     
+
+class Courses(models.Model):
+    id=models.AutoField(primary_key=True)
+    course_name=models.CharField(max_length=40)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updates_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class Subjects(models.Model):
+    id=models.AutoField(primary_key=True)
+    subject_name=models.CharField(max_length=40)
+    course_id=models.ForeignKey(Courses,on_delete=models.CASCADE)
+    staff_id=models.ForeignKey(Staffs,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+
+
+class Students(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    password=models.CharField(max_length=50)
+    gender=models.CharField(max_length=30)
+    profile_pic=models.FileField()
+    address=models.TextField()
+    course_id=models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+
+
+class Attendance(models.Model):
+    id=models.AutoField(primary_key=True)
+    subject_id=models.ForeignKey(Subjects,on_delete=models.DO_NOTHING)
+    attendance_date=models.DateTimeField(auto_now_add=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+class AttendanceReport(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    attendance_id=models.ForeignKey(Attendance,on_delete=models.CASCADE)
+    status=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class LeaveReportStudent(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    leave_date=models.CharField(max_length=60)
+    leave_message=models.TextField()
+    leave_status=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class LeaveReportStaff(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    leave_date=models.CharField(max_length=60)
+    leave_message=models.TextField()
+    leave_status=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
