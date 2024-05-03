eede44e2b33c562b5ebda3f79b68efc7de0db99c
salmonGowa <gowasalmon@yahoo.com>
2024-05-03 11:59:19 -0400

    config

--- schoolmanagement/schoolmanagement/settings.py
+++ schoolmanagement/schoolmanagement/settings.py
@@ -116,8 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 STATIC_URL = 'static/'
